3c9c694f95e3246373b6ca081a3776e744707d9d
Silvan Groenveld <151916823+Siilvan@users.noreply.github.com>
2024-05-17 13:03:10 +0200

    Add files via upload

---
+++ install.lua
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/C-Christiaan/TechTitans/main/house.lua?token=GHSAT0AAAAAACSHJQ5E2BEM6XRGGGX7PCR2ZSHIVJA
+sleep(1)
+wget https://raw.githubusercontent.com/C-Christiaan/TechTitans/main/startupturtle.lua?token=GHSAT0AAAAAACSHJQ5EKCJI63CFF6ARF4GMZSHIV2A startup.lua
+os.reboot()
---
+++ installchatbox.lua
@@ -0,0 +1,4 @@
+wget https://raw.githubusercontent.com/C-Christiaan/TechTitans/main/startup.lua?token=GHSAT0AAAAAACSHJQ5E2IOWUPX6JOJ76HF4ZSHIZGA
+sleep(1)
+wget https://raw.githubusercontent.com/C-Christiaan/TechTitans/main/listener.lua?token=GHSAT0AAAAAACSHJQ5EQQO7NTXJ2EBJIMQEZSHI3MA
+os.reboot()
